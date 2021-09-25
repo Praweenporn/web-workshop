d29d18dd302ac662c0347339265dab2af748a61e
Praweenporn <71056576+Praweenporn@users.noreply.github.com>
2021-09-25 15:20:33 +0700

    added: new vue project

---
+++ webpalo/src/components/HelloWorld.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg1 }}</h1>
+    <h2>{{ name }}</h2>
+    <br/><button @click="counter += 1">Add 1</button><br/>
+  <p>The button above has been clicked {{ counter }} times.</p>
+  </div>
+  
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg1: String,
+    name: String,
+  },
+    data() {
+    return {
+      counter: 0
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
+
+
